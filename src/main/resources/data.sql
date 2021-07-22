48a91eeb885904d4643eb92f2fb0384c24ff4e40
jonathan <jonathan@jonathan.com>
2021-07-22 19:08:28 -0300

    adicionado inicializacao automatica de dados

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into public.tb_video (titulo, descricao, url)
+values 
+('Microserviços','o que sao de onde vem e o que comem','https://www.youtube.com/watch?v=jSnLOoGjQ80'),
+('Emuladores','sempre curti','https://www.youtube.com/watch?v=9qx7qjKhJ1Q'),
+('Alura Cases','curioso pra assistir','https://www.youtube.com/watch?v=Amt8gqUCHB8');
